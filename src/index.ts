d14ebc4e0f059e2e7d000357d76298529446984d
Duncan Cross <duncan.cross@gmail.com>
2020-05-30 19:16:27 +0100

    Add 3 basic expression types

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,29 @@
 
 import { Observable } from 'rxjs';
+
+export const enum ExpressionType {
+  LITERAL = 'literal',
+  OPERATOR = 'operator',
+  SCOOP_BLOCK = 'scoopBlock',
+}
+
+export interface Literal {
+  type: ExpressionType.LITERAL;
+  literal: string;
+}
+
+export interface Operator {
+  type: ExpressionType.OPERATOR;
+  operator: string;
+  operands: Observable<Expression>;
+}
+
+export interface ScoopBlock {
+  type: ExpressionType.SCOOP_BLOCK;
+  blockType: string;
+  scoops: Observable<Observable<Expression>>;
+
+  hintInfix?: boolean;
+}
+
+export type Expression = Literal | Operator | ScoopBlock;
